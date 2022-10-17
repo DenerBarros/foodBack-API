054ac29059c205c4b26620775dde3bea25490553
Dener Marcos Tolentino Barros - Unicred Evolução <dener.4022@e-unicred.com.br>
2022-10-17 16:50:49 -0300

    Criado as rotas de autenticacao definido o middleware para JWT definido as configurações do Auth

---
+++ src/controllers/UserControllers.js
@@ -0,0 +1,33 @@
+const { hash } = require('bcryptjs');
+
+const AppError = require('../utils/AppError');
+
+const sqliteConnection = require('../database/sqlite');
+
+class UserController {
+  async create(request, response) {
+    const { name, email, password } = request.body;
+
+    const database = await sqliteConnection();
+
+    const checkUserExists = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError('Este e-mail ja está em uso');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, hashedPassword]
+    );
+
+    return response.status(201).json();
+  }
+}
+
+module.exports = UserController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
 const userRoutes = require('./users.routes');
+const sessionsRoutes = require('./sessions.routes');
 
 const routes = Router();
 
 routes.use('/users', userRoutes);
-
+routes.use('/sessions', sessionsRoutes);
 
 module.exports = routes;
